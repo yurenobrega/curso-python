c66076f3f90af3082a5b11873738bbb67d956ef8
yure <yurenobrega221b@gmail.com>
2024-03-27 07:54:07 -0300

    Lists

---
+++ 15_lists/main.py
@@ -0,0 +1,13 @@
+food = ["pizza", "hamburger", "hotdog", "spaghetti", "pudding"]
+
+food[0] = "sushi"
+
+food.append("ice cream")
+food.remove("hotdog")
+food.pop()
+food.insert(0, "cake")
+food.sort()
+# food.clear()
+
+for x in food:
+    print(x)
