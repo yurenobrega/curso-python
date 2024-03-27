1447c4e1077bbfb91aea9d1069b72aa1181ca727
yure <yurenobrega221b@gmail.com>
2024-03-27 12:27:58 -0300

    Funções

---
+++ 21_functions/main.py
@@ -0,0 +1,10 @@
+def hello(name, lastName, age):
+    print("Hello " + name + " " + lastName)
+    print("You are " + str(age) + " years old")
+    print("Have a nice day!")
+
+my_name = "Yure"
+last_name = "Nóbrega"
+
+hello(my_name, last_name, 22)
+
