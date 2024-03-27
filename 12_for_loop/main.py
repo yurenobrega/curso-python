3d4d5f584500b92abbb226204ac5ddfbdf3c1aac
yure <yurenobrega221b@gmail.com>
2024-03-26 23:35:53 -0300

    For loop

---
+++ 12_for_loop/main.py
@@ -0,0 +1,12 @@
+import time 
+
+for i in range(1, 11):
+    print(i)
+    
+for i in "Yure Nóbrega":
+    print(i)
+    
+for seconds in range(10, 0, -1):
+    print(seconds)
+    time.sleep(1)
+print("Happy new year!")
