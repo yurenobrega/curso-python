01aaf09136ef0d976b482a58a8fd073820a2cdd6
yure <yurenobrega221b@gmail.com>
2024-03-27 09:56:32 -0300

    Indexação de string

---
+++ 20_indexing/main.py
@@ -0,0 +1,12 @@
+name = "yure Nóbrega!"
+
+#if (name[0].islower()):
+    #name = name.capitalize()
+
+first_name = name[:4].capitalize()
+last_name = name[5:12]
+last_character = name[-1]
+
+print(first_name, end=" ")
+print(last_name, end="")
+print(last_character)
