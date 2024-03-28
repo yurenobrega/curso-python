7dbce9f2f66d135a3e53e931e82e98ba2f7a0b9e
yure <yurenobrega221b@gmail.com>
2024-03-28 15:36:56 -0300

    Módulo random

---
+++ 29_random_numbers/main.py
@@ -0,0 +1,17 @@
+import random
+
+x = random.randint(1, 6)
+y = random.random() 
+
+myList = ['rock', 'paper', 'scissors']
+z = random.choice(myList)
+
+cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, "J", "Q", "K", "A"]
+
+print(x)
+print(y)
+print(z)
+
+random.shuffle(cards)
+
+print(cards)
