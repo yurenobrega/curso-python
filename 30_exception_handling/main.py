1f9ca106bd18f8863f78569178d60e1cbd7fcf6f
yure <yurenobrega221b@gmail.com>
2024-03-28 15:49:01 -0300

    Tratamento de exceções

---
+++ 30_exception_handling/main.py
@@ -0,0 +1,16 @@
+try:  
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+except ZeroDivisionError as e:
+    print(e)
+    print("You can't divide by zero!")
+except ValueError as e:
+    print(e)
+    print("Enter only numbers!")
+except Exception as e:
+    print(f"Something went wrong: {e}")
+else:
+    print(result)
+finally:
+    print("This will always execute")
