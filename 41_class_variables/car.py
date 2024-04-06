cb029af1af38e4965e4e10b0a86f3dc340f82b3a
yure <yurenobrega221b@gmail.com>
2024-04-06 00:17:40 -0300

    Variável de classe

---
+++ 41_class_variables/car.py
@@ -0,0 +1,11 @@
+class Car:
+    
+    wheels = 4 #class variable
+    
+    def __init__(self, make, model, year, color):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.color = color
+
+    
